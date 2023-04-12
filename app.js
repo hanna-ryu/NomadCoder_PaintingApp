fd5cffe5ae49a32072e540c7b39616934225b1ce
Hanna <foureaf@gmail.com>
2023-04-13 08:08:50 +0900

    finished

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const ctx = canvas.getContext("2d");
 const lineWidth = document.getElementById("line-width");
 const color = document.querySelector("#color");
 const fileInput = document.getElementById('file');
+const textInput = document.getElementById('text');
+const saveBtn = document.getElementById("save");
+
 
 const colorOptions = Array.from(
     document.getElementsByClassName("color-option")
@@ -18,6 +21,7 @@ const CANVAS_HEIGHT = 800;
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round"
 
 let isPainting = false;
 let isFilling = false;
@@ -89,9 +93,29 @@ function onFileChange(event){
         ctx.drawImage(image,0,0, 800,800);
         fileInput.value = null;
     }
+}
+
+function onDoubleClick(event){
+    const text = textInput.value
+    if(text !== ""){
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font=" 50px serif"
+    ctx.strokeText(text, event.offsetX, event.offsetY);
+    ctx.restore();
+    }
+}
+
+function onSaveClick(){
+    const url = canvas.toDataURL();
+    const a = document.createElement('a');
+    a.href = url ;
+    a.download = "myDrawing.png";
+    a.click();
 
 }
 
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -108,3 +132,4 @@ modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
 eraseBtn.addEventListener("click", onEraseClick);
 fileInput.addEventListener("change",onFileChange);
+saveBtn.addEventListener("click", onSaveClick)
